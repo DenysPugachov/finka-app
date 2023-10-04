a599d6e05532ee18b2bbfe077a42e25dd20ec0f7
Denys Pugachov <pugachovdenys@gmail.com>
2023-10-04 08:49:45 +0200

    0: clear project

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,7 @@
-import Button from "./Button";
-import ExpensesList from "./ExpensesList";
-import FormAddExpense from "./FormAddExpense";
-
-const expensesArr = [
-  {
-    id: 1,
-    title: "🍎 Food",
-    // image: "🍎",
-  },
-  {
-    id: 2,
-    title: "👕 Close",
-    // image: "👕",
-  },
-  {
-    id: 3,
-    title: "🧴 Cosmetics",
-    // image: "🧴",
-  },
-];
 
 export default function App() {
   return (
-    <div className="app">
-      <div className="sidebar">
-
-        <ExpensesList expensesArr={expensesArr} />
-
-        <FormAddExpense />
-
-        <Button>
-          Add New Expense
-        </Button>
-      </div>
-    </div>
+    <div>New app template</div>
   );
 }
 
