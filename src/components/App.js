d13dffc4458edac6391d0672fed9192ded052c8f
Denys Pugachov <pugachovdenys@gmail.com>
2023-10-05 14:32:23 +0200

    add: ui with panel buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,75 @@
+const deposits = [
+  {
+    id: 1,
+    title: "Den's Card",
+    amount: 123.59
+  },
+  {
+    id: 2,
+    title: "Jane's Card",
+    amount: 23.59
+  },
+  {
+    id: 3,
+    title: "Den's Cash",
+    amount: 1523.59
+  },
+  {
+    id: 4,
+    title: "Jane's Cash",
+    amount: 333.59
+  },
+  {
+    id: 5,
+    title: "Other",
+    amount: 0,
+  },
+  {
+    id: 0,
+    title: "➕",
+    amount: ""
+  }
+]
+
 
 export default function App() {
   return (
-    <div>New app template</div>
+    <div className="app">
+
+      <div className="panel panel__balance">
+        <h2 className="panel__title">Balance: _222.22zl.</h2>
+        <div className="panel__content">
+          {deposits.map(data =>
+            <DepositItem key={data.id} data={data} />)}
+        </div>
+      </div>
+
+      <div className="panel panel__expenses">
+        <h2 className="panel__title">Expenses: _222.22zl.</h2>
+        <div className="panel__content">
+          {deposits.map(data =>
+            <DepositItem key={data.id} data={data} />)}
+        </div>
+      </div>
+
+      <form className="input-container">
+        <input type="number" placeholder="Amount here..." />
+        <button type="submit" className="button">✓</button>
+      </form>
+
+    </div>
   );
 }
+
+function DepositItem({ data }) {
+  const { title, amount } = data
+  return (
+    <button className="button">
+      <h5>{title}</h5>
+      {amount}zl.
+    </button>
+  )
+}
+
 
 
